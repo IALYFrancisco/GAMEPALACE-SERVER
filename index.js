58d1994d0f49e014fb43691472043ebebca60f61
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-02-02 21:14:26 +0300

    mise en place du middleware que permet de donner des autorisations CORS envers le serveur tel que les autorisations methods, oigin, headers

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import e from 'express'
 import dotenv from 'dotenv'
 import userRouter from './src/routes/userRoutes.js'
+import { corsAuthorization } from './src/middlewares/index.js'
 
 dotenv.config()
 
@@ -8,6 +9,8 @@ dotenv.config()
 const app = e()
 
 app.use(e.json())
+
+app.use(corsAuthorization)
 
 app.get('/', (request, response) => {
     response.status(200).json("Ready to start")
