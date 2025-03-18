01da3b710c99b811e05c68d8c8f5c1ffcc8bb63c
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-03-18 20:31:36 +0300

    modification du modèle de donnée des utilisateurs

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -4,7 +4,8 @@ const userSchema = mongoose.Schema({
     name: {type: String, required: true},
     birthdate: {type: String, required: true},
     email: { type: String, required: true},
-    password: {type: String, required: true}
+    password: {type: String, required: true},
+    accessToken: {type: String, default: ""}
 })
 
 const userCollection = new mongoose.model('Users', userSchema)
