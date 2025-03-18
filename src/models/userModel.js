fd9b9a2c662ab7149f89a3fc023bc303e8bfcd90
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-03-18 20:32:57 +0300

    définition du valeur par défaut du champ acessToken en null

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -5,7 +5,7 @@ const userSchema = mongoose.Schema({
     birthdate: {type: String, required: true},
     email: { type: String, required: true},
     password: {type: String, required: true},
-    accessToken: {type: String, default: ""}
+    accessToken: {type: String, default: null}
 })
 
 const userCollection = new mongoose.model('Users', userSchema)
