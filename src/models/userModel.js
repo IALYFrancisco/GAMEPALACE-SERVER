e5a6742cfbc650a228fb8f5b9c94c3885949e663
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-05-04 16:17:55 +0300

    suppréssion de l'attrobut accessToken dans le modèle de données utilisateurs

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -5,7 +5,6 @@ const userSchema = mongoose.Schema({
     birthdate: {type: String, required: true},
     email: { type: String, required: true},
     password: {type: String, required: true},
-    accessToken: {type: String, default: null},
     register_date: { type: Date, required: true, default: Date.now }
 })
 
