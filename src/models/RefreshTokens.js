5f2e7d497e95d0f8c8acee64c4b6e6b7ad791b38
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-05-04 16:21:31 +0300

    exportation du modèle de données refreshtokens en tant que module

--- src/models/RefreshTokens.js
+++ src/models/RefreshTokens.js
@@ -3,3 +3,7 @@ import mongoose from 'mongoose'
 const refreshTokensSchema = mongoose.Schema({
     token: { type: String, required: true }
 })
+
+const RefreshTokens = new mongoose.model('RefreshTokens', refreshTokensSchema)
+
+export default RefreshTokens
