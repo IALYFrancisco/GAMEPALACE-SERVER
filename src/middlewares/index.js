539c527fbb5d702c2a29774ab89a6467070c5deb
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-05-06 13:23:51 +0300

    ajout d'une autorisation cors et d'une fonction log

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -2,5 +2,6 @@ export function corsAuthorization(request, response, next){
     response.setHeader('Access-Control-Allow-Origin', process.env.ALLOWED_ORIGIN)
     response.setHeader('Access-Control-Allow-Method', 'POST')
     response.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+    response.setHeader('Access-Control-Allow-Credentials', true)
     next()
 }
