b95a29fd19263a975c4d764ad6e77462f4172f15
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-06-26 20:06:22 +0300

    suppression de lignes vides

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -4,19 +4,14 @@ import { hashUserPassword, userPasswordVerify } from "./othersServices.js";
 import jsonwebtoken from "jsonwebtoken";
 
 export async function getAllUser( request, response ){
-    
     await dbConnexion()
-
     let allUser = await userCollection.find({})
-
     response.set('Content-Type', 'application/json')
-
     if(allUser == ""){
         response.status(404).json("Resource not found, the collection is empty")
     }else {
         response.status(200).json(allUser)
     }
-
     await dbDisconnexion()
 }
 
